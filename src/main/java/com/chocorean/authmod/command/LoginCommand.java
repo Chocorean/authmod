42445b00c6f859d4cfca58051b22e9b5b1915a88
Baptiste CHOCOT <baptiste.chocot@telecomnancy.net>
2018-02-28 16:51:52 +0100

    added forst version of login command class

---
+++ src/main/java/com/chocorean/authmod/command/LoginCommand.java
@@ -0,0 +1,65 @@
+package com.chocorean.authmod.command;
+
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginCommand implements ICommand {
+    private final ArrayList aliases;
+
+    public LoginCommand(){
+        aliases = new ArrayList();
+        aliases.add("login");
+        aliases.add("log");
+    }
+
+    @Override
+    public String getCommandName() {
+        return "login";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
+        return "/login <password> - Allows you to play.";
+    }
+
+    @Override
+    public List<String> getCommandAliases() {
+        return aliases;
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        World world = sender.getEntityWorld();
+        if (!world.isRemote) { // Server side
+            // coming
+        }
+    }
+
+    @Override
+    public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+        return true;
+    }
+
+    @Override
+    public List<String> getTabCompletionOptions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos pos) {
+        return new ArrayList<String>();
+    }
+
+    @Override
+    public boolean isUsernameIndex(String[] args, int index) {
+        return true;
+    }
+
+    @Override
+    public int compareTo(ICommand iCommand) {
+        return this.getCommandName().compareTo(iCommand.getCommandName());
+    }
+}
