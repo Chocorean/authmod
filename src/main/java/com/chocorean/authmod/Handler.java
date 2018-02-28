eb6b54f91e164fbe5339ae46e660ff8bbdaaca1c
Baptiste CHOCOT <baptiste.chocot@telecomnancy.net>
2018-02-28 20:13:13 +0100

    blocks players attempting to use a command or play while they are not logged or registered

--- src/main/java/com/chocorean/authmod/Handler.java
+++ src/main/java/com/chocorean/authmod/Handler.java
@@ -5,23 +5,57 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
+import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.LinkedList;
 
+@Mod.EventBusSubscriber
 public class Handler {
-    public static LinkedList<Thread> threads = new LinkedList<>();
+    public static LinkedList<PlayerDescriptor> desc = new LinkedList();
+
+    @SubscribeEvent(priority= EventPriority.HIGHEST)
+    public static void onJoin(EntityJoinWorldEvent event){
+        Entity entity = event.getEntity();
+        if (entity instanceof EntityPlayer && !entity.isDead) {
+            ((EntityPlayer) entity).addChatComponentMessage(new TextComponentString("hello"));
+            World world = entity.getEntityWorld();
+            BlockPos pos = entity.getPosition();
+            if (!world.isRemote){
+                PlayerDescriptor dc = new PlayerDescriptor((EntityPlayer)entity, pos);
+                desc.add(dc);
+            }
+        }
+    }
+
+    @SubscribeEvent(priority=EventPriority.HIGHEST)
+    public static void onPlayerEvent(PlayerEvent event) {
+        for (PlayerDescriptor dc : Handler.desc) {
+            Entity entity = event.getEntity();
+            World world = entity.getEntityWorld();
+            if (dc.getPlayer().getName().equals(entity.getName())) {
+                // player still havent logged in successfully
+                if (event.isCancelable()) {
+                    event.setCanceled(true);
+                    entity.addChatMessage(new TextComponentString("You have to use /register ou /login to play."));
+                }
+            }
+        }
+    }
 
     @SubscribeEvent
-    public void onJoin(EntityJoinWorldEvent event){
-        Entity entity = event.getEntity();
-        World world = event.getWorld();
-        BlockPos pos = entity.getPosition();
-        if (entity instanceof EntityPlayer && !entity.isDead) {
-            if (!world.isRemote){
-                threads.add(new Thread(new BlockThread((EntityPlayer)entity, pos)));
-                threads.getLast().start();
+    public static void onCommand(CommandEvent event){
+        if (!(event.getSender() instanceof EntityPlayer))
+            return;
+        String name = event.getCommand().getCommandName();
+        if (!(name.equals("register") || name.equals("login"))) {
+            if (event.isCancelable()){
+                event.setCanceled(true);
+                event.getSender().addChatMessage(new TextComponentString("You have to use /register ou /login to use commands."));
             }
         }
     }
