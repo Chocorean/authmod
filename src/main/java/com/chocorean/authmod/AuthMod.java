560ce001d24a7079e4a9ab7603aaa6cbd1dfa508
Baptiste CHOCOT <baptiste.chocot@telecomnancy.net>
2018-02-28 20:11:36 +0100

    added name, serverSideOnly

--- src/main/java/com/chocorean/authmod/AuthMod.java
+++ src/main/java/com/chocorean/authmod/AuthMod.java
@@ -6,16 +6,20 @@ import com.chocorean.authmod.proxy.CommonProxy;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.logging.Logger;
 
-@Mod(modid = AuthMod.MODID, version = AuthMod.VERSION, serverSideOnly = true)
+@Mod(modid = AuthMod.MODID, name = AuthMod.NAME, version = AuthMod.VERSION, serverSideOnly = true, acceptableRemoteVersions = "*")
 public class AuthMod {
     public static final String MODID = "authmod";
+    public static final String NAME = "AuthMod";
     public static final String VERSION = "1.0";
     public static final String COMMON_PROXY = "com.chocorean.authmod.proxy.CommonProxy";
     public static final String CLIENT_PROXY = "com.chocorean.authmod.proxy.ClientProxy";
@@ -23,8 +27,10 @@ public class AuthMod {
     @SidedProxy(clientSide = AuthMod.CLIENT_PROXY, serverSide = AuthMod.COMMON_PROXY)
     public static CommonProxy proxy;
 
+    public static final Logger LOGGER = Logger.getLogger(AuthMod.MODID);
+
     @Mod.EventHandler
-    public void preload(FMLPreInitializationEvent event) {
+    public void preInit(FMLPreInitializationEvent event) {
         File dir = new File("mods/AuthMod");
         dir.mkdir();
         if (!new File("mods/AuthMod/data").exists()) {
@@ -39,9 +45,18 @@ public class AuthMod {
     }
 
     @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {}
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {}
+
+    @Mod.EventHandler
     public void serverStarting(FMLServerStartingEvent event) {
+        LOGGER.info("Registering AuthMod Event Handler");
         MinecraftForge.EVENT_BUS.register(new Handler());
+        LOGGER.info("Registering AuthMod Register Command");
         event.registerServerCommand(new RegisterCommand());
+        LOGGER.info("Registering AuthMod Login Handler");
         event.registerServerCommand(new LoginCommand());
     }
 }
