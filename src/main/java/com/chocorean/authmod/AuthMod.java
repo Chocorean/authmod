4c4ad28f7425375d28636a50c5b0907353cbad82
Baptiste CHOCOT <baptiste.chocot@telecomnancy.net>
2018-02-28 16:50:28 +0100

    added serverSideOnly tag

--- src/main/java/com/chocorean/authmod/AuthMod.java
+++ src/main/java/com/chocorean/authmod/AuthMod.java
@@ -13,10 +13,9 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintWriter;
 
-@Mod(modid = AuthMod.MODID, version = AuthMod.VERSION)
+@Mod(modid = AuthMod.MODID, version = AuthMod.VERSION, serverSideOnly = true)
 public class AuthMod {
     public static final String MODID = "authmod";
     public static final String VERSION = "1.0";
