c8699c46ff714976d75bb080e5233d22decd03cb
Baptiste CHOCOT <baptiste.chocot@telecomnancy.net>
2018-02-28 16:54:50 +0100

    added proxies

---
+++ src/main/java/com/chocorean/authmod/proxy/ClientProxy.java
@@ -0,0 +1,4 @@
+package com.chocorean.authmod.proxy;
+
+public class ClientProxy extends CommonProxy {
+}
---
+++ src/main/java/com/chocorean/authmod/proxy/CommonProxy.java
@@ -0,0 +1,6 @@
+package com.chocorean.authmod.proxy;
+
+import net.minecraftforge.fml.common.event.*;
+
+public class CommonProxy {
+}
