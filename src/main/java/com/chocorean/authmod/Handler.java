eb33928ac8eb94825365ec34be10844d716d3a84
Baptiste CHOCOT <baptiste.chocot@telecomnancy.net>
2018-03-01 11:08:57 +0100

    now kick unlogged players after 1min

--- src/main/java/com/chocorean/authmod/Handler.java
+++ src/main/java/com/chocorean/authmod/Handler.java
@@ -2,6 +2,7 @@ package com.chocorean.authmod;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
@@ -13,6 +14,8 @@ import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.LinkedList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @Mod.EventBusSubscriber
 public class Handler {
@@ -20,8 +23,23 @@ public class Handler {
 
     @SubscribeEvent(priority= EventPriority.HIGHEST)
     public static void onJoin(EntityJoinWorldEvent event){
-        Entity entity = event.getEntity();
+        final Entity entity = event.getEntity();
         if (entity instanceof EntityPlayer && !entity.isDead) {
+            // initializing timer for kicking player if he/she hasn't logged in a minute
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    for (PlayerDescriptor dc : desc){
+                        if (dc.getPlayer().getName().equals(entity.getName())) {
+                            desc.remove(dc);
+                            // back to connection position and kicked
+                            ((EntityPlayerMP)entity).setPositionAndUpdate(dc.getPos().getX(),dc.getPos().getY(),dc.getPos().getZ());
+                            ((EntityPlayerMP)entity).connection.kickPlayerFromServer("You took too many time to log in.");
+                        }
+                    }
+                }
+            },60000);
             World world = entity.getEntityWorld();
             BlockPos pos = entity.getPosition();
             if (!world.isRemote){
