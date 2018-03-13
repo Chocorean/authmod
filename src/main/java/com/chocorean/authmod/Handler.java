9e3162b3a304d16aa11bb628a108ddbef9ed37ef
chocorean <baptiste.chocot@gmail.com>
2018-03-13 08:39:15 +0100

    fixed: ask for logging each time a player changes dimension

--- src/main/java/com/chocorean/authmod/Handler.java
+++ src/main/java/com/chocorean/authmod/Handler.java
@@ -3,11 +3,11 @@ package com.chocorean.authmod;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.play.server.SPacketChat;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.event.CommandEvent;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -22,43 +22,38 @@ public class Handler {
     public static LinkedList<PlayerDescriptor> desc = new LinkedList();
 
     @SubscribeEvent(priority= EventPriority.HIGHEST)
-    public static void onJoin(EntityJoinWorldEvent event){
-        final Entity entity = event.getEntity();
-        if (entity instanceof EntityPlayer && !entity.isDead) {
-            // initializing timer for kicking player if he/she hasn't logged in a minute
-            Timer timer = new Timer();
-            timer.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    for (PlayerDescriptor dc : desc){
-                        if (dc.getPlayer().getName().equals(entity.getName())) {
-                            desc.remove(dc);
-                            // back to connection position and kicked
-                            ((EntityPlayerMP)entity).setPositionAndUpdate(dc.getPos().getX(),dc.getPos().getY(),dc.getPos().getZ());
-                            ((EntityPlayerMP)entity).connection.kickPlayerFromServer("You took too many time to log in.");
-                        }
+    public static void onJoin(net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent event){
+        final EntityPlayer entity = event.player;
+        // initializing timer for kicking player if he/she hasn't logged in a minute
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                for (PlayerDescriptor dc : desc){
+                    if (dc.getPlayer().getName().equals(entity.getName())) {
+                        desc.remove(dc);
+                        // back to connection position and kicked
+                        entity.setPositionAndUpdate(dc.getPos().getX(),dc.getPos().getY(),dc.getPos().getZ());
+                        ((EntityPlayerMP)entity).connection.kickPlayerFromServer("You took too many time to log in.");
                     }
                 }
-            },60000);
-            World world = entity.getEntityWorld();
-            BlockPos pos = entity.getPosition();
-            if (!world.isRemote){
-                PlayerDescriptor dc = new PlayerDescriptor((EntityPlayer)entity, pos);
-                desc.add(dc);
             }
-        }
+        },60000);
+        World world = entity.getEntityWorld();
+        BlockPos pos = entity.getPosition();
+        PlayerDescriptor dc = new PlayerDescriptor(entity, pos);
+        desc.add(dc);
     }
 
     @SubscribeEvent(priority=EventPriority.HIGHEST)
     public static void onPlayerEvent(PlayerEvent event) {
         for (PlayerDescriptor dc : Handler.desc) {
             Entity entity = event.getEntity();
-            World world = entity.getEntityWorld();
             if (dc.getPlayer().getName().equals(entity.getName())) {
                 // player still havent logged in successfully
                 if (event.isCancelable()) {
                     event.setCanceled(true);
-                    entity.addChatMessage(new TextComponentString("You have to use /register ou /login to play."));
+                    ((EntityPlayerMP)entity).connection.sendPacket(new SPacketChat(new TextComponentString("You have to use /register ou /login to play.")));
                 }
             }
         }
@@ -76,7 +71,7 @@ public class Handler {
                 if (dc.getPlayer().getName().equals(entity.getName())) {
                     if (event.isCancelable()){
                         event.setCanceled(true);
-                        event.getSender().addChatMessage(new TextComponentString("You have to use /register ou /login to use commands."));
+                        ((EntityPlayerMP)event.getSender()).connection.sendPacket(new SPacketChat(new TextComponentString("You have to use /register ou /login to use commands.")));
                     }
                 }
             }
