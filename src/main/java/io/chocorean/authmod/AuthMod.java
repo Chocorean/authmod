7e23bf0cec3fe4f499c248ac9e8aabadc06d88ec
Yann Prono <yann.prono@nist.gov>
2019-01-29 00:51:52 -0500

    update authmod's version

--- src/main/java/io/chocorean/authmod/AuthMod.java
+++ src/main/java/io/chocorean/authmod/AuthMod.java
@@ -25,7 +25,7 @@ public class AuthMod {
 
     static final String MODID = "authmod";
     static final String NAME = "AuthMod";
-    public static final String VERSION = "2.6";
+    public static final String VERSION = "2.7";
     private static final String COMMON_PROXY = "io.chocorean.authmod.proxy.CommonProxy";
     private static final String CLIENT_PROXY = "io.chocorean.authmod.proxy.ClientProxy";
     public static Logger LOGGER = FMLLog.log;
