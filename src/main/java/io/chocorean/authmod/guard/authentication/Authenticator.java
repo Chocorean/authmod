58ef75f9e9e3d9219c7d976062990f4b92f5c8dd
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2020-03-21 15:17:33 -0400

    Remove warning

--- src/main/java/io/chocorean/authmod/guard/authentication/Authenticator.java
+++ src/main/java/io/chocorean/authmod/guard/authentication/Authenticator.java
@@ -1,18 +1,16 @@
 package io.chocorean.authmod.guard.authentication;
 
-import org.apache.logging.log4j.Logger;
 import org.mindrot.jbcrypt.BCrypt;
 
-import io.chocorean.authmod.AuthMod;
 import io.chocorean.authmod.exception.*;
 import io.chocorean.authmod.guard.datasource.IDataSourceStrategy;
+import io.chocorean.authmod.guard.payload.IPayload;
 import io.chocorean.authmod.guard.payload.LoginPayload;
 import io.chocorean.authmod.model.IPlayer;
 
 import javax.validation.ConstraintViolation;
 
 public class Authenticator {
-  private static final Logger LOGGER = AuthMod.LOGGER;
   private final IDataSourceStrategy dataSource;
 
   public Authenticator(IDataSourceStrategy dataSourceStrategy) {
@@ -34,7 +32,7 @@ public class Authenticator {
           throw new WrongPasswordException();
         return true;
       } else {
-        for (ConstraintViolation c : payload.getErrors()) {
+        for (ConstraintViolation<IPayload> c : payload.getErrors()) {
           if (c.getPropertyPath().toString().equals("email")) throw new InvalidEmailException();
           if (c.getPropertyPath().toString().equals("password"))
             throw new WrongPasswordException();
