8f2cda75aa437c9d45b6cce79a17a61c1bad8b71
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2020-04-21 08:59:46 -0400

    Add unit test for previous bugfix (Fix #23)

--- src/test/java/io/chocorean/authmod/command/RegisterCommandTest.java
+++ src/test/java/io/chocorean/authmod/command/RegisterCommandTest.java
@@ -87,11 +87,38 @@ class RegisterCommandTest {
 
   @Test
   void testExecuteAlreadyLogged() {
-    handler.authorizePlayer(playerEntity);
+    handler.authorizePlayer(this.playerEntity);
     assertTrue(this.handler.isLogged(this.playerEntity));
     int res = RegisterCommand.execute(this.source, this.handler, this.guard, this.payload);
     assertEquals(1, res);
     assertTrue(this.handler.isLogged(this.playerEntity));
   }
 
+  @Test
+  void testBugFix3_2() throws Exception {
+    /*
+     * Context:
+     *     Strategy: FILE
+     *     emailRequired: False
+     * How to reproduce:
+     *     register once person successfully
+     *     try to register a second one
+     * Expected message: You are already logged in.
+     */
+    // Init
+    PlayerInterface secondPlayer = new Player("Superman", "094b0779-992c-4fac-90f5-fa839dc77dbc");
+    ServerPlayerEntity secondPlayerEntity = mock(ServerPlayerEntity.class);
+    when(secondPlayerEntity.getGameProfile()).thenReturn(new GameProfile(UUID.fromString(secondPlayer.getUuid()), secondPlayer.getUsername()));
+    when(secondPlayerEntity.getDisplayName()).thenReturn(new StringTextComponent(secondPlayer.getUsername()));
+    PayloadInterface secondPayload = new Payload(secondPlayer, new String[]{"sliprouge", "sliprouge"});
+    
+    // Scenario
+    this.handler.authorizePlayer(this.playerEntity);
+    assertTrue(this.handler.isLogged(this.playerEntity));
+    CommandSource secondSource = mock(CommandSource.class);
+    when(secondSource.asPlayer()).thenReturn(secondPlayerEntity);
+    int res = RegisterCommand.execute(secondSource, this.handler, this.guard, secondPayload);
+    assertEquals(1, res);
+    assertTrue(this.handler.isLogged(secondPlayerEntity));
+  }
 }
